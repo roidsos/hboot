2d456105e9b7fd733f55b9594fcaf479b4b4d2c6
lolguy91 <retek1544@gmail.com>
2024-08-03 12:00:30 +0200

    asd

--- src/arch/x86_64-pc/boot.c
+++ src/arch/x86_64-pc/boot.c
@@ -3,7 +3,7 @@
 EFI_STATUS boot_entry(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello from Hboot!\r\n");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"TODO: bootload!\r\n");
     for (;;)
         ;
 }
