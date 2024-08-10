b1cd42fbaf6467bcc76a5a6e3daa16db472ac779
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-08-10 15:59:22 +0200

    Typo preventing compilation

--- src/arch/x86_64-pc/boot.c
+++ src/arch/x86_64-pc/boot.c
@@ -10,7 +10,7 @@ EFI_STATUS boot_entry(EFI_HANDLE _ih, EFI_SYSTEM_TABLE *_st)
     IH = _ih;
     ST = _st;
 
-    if(file_init() != HB_SUCESS){
+    if(file_init() != HB_SUCCESS){
         _st->ConOut->OutputString(_st->ConOut, L"Failed to init the file system!\r\n");
         for (;;)
             ;
